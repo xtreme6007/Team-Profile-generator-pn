c3a061315bfd4fb00be60e3c6e704af18a112299
xtreme6007 <prestonnichols96@gmail.com>
2020-09-01 16:28:21 -0500

    added files nd working on Employee class

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,25 @@
+// TODO: Write code to define and export the Employee class
+
+class Employee {
+   
+    constructor(id, name, email) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+    }
+    getName() {
+
+    }
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getRole() {
+        return "Employee"
+    }
+
+  }
