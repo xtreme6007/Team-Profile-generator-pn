3039cebe67553d274e9fcb7fb6c01f9ae47f5675
xtreme6007 <prestonnichols96@gmail.com>
2020-09-02 11:30:07 -0500

    required Employee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,10 +1,11 @@
 // TODO: Write code to define and export the Employee class
 
+
 class Employee {
     constructor(id, name, email) {
-      this.id = id;
-      this.name = name;
-      this.email = email;
+        this.id = id;
+        this.name = name;
+        this.email = email;
     }
     getName() {
         return this.name
@@ -14,12 +15,13 @@ class Employee {
     }
 
     getEmail() {
-return this.email
+        return this.email
     }
 
     getRole() {
         return "Employee";
     }
 
-  }
-  const Employee = new Employee(52,"ron","cscsdcsdcsad@dsmad.com");
+}
+
+
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,5 +1,7 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-class Manager extends Employee{
+const Employee = require("./Employee")
+
+class Manager extends Employee {
 constructor(officeNumber){
     super(id, name, email)
 this.officeNumber = officeNumber
