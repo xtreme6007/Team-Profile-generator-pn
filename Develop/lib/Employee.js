a5007419f2a07b3ca3a22cd2c3e6cb70f763d274
xtreme6007 <prestonnichols96@gmail.com>
2020-09-02 08:35:26 -0500

    added enginner class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,14 +1,13 @@
 // TODO: Write code to define and export the Employee class
 
 class Employee {
-   
     constructor(id, name, email) {
       this.id = id;
       this.name = name;
       this.email = email;
     }
     getName() {
-
+        
     }
     getId() {
 
@@ -19,7 +18,8 @@ class Employee {
     }
 
     getRole() {
-        return "Employee"
+        return "Employee";
     }
 
   }
+  const Employee = new Employee(52,"ron","cscsdcsdcsad@dsmad.com");
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(github){
+        super(id, name, email)
+        this.githubUsername = github;
+    }
+    getGithub() {
+        const gitProfile = "https://github.com/" + this.github;
+        return gitProfile;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+}
