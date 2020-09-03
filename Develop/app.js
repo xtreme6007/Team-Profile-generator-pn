156470ee1b033661a2c134be7d0b8f144f355db9
xtreme6007 <prestonnichols96@gmail.com>
2020-09-03 11:57:16 -0500

    working on rendering html completed inquerier to work correctly

--- Develop/app.js
+++ Develop/app.js
@@ -4,14 +4,18 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-function addMember() {
-    const Answers =  inquirer.prompt([{
+
+let answers ={};
+
+
+
+ async function addMember() {
+     answers = await inquirer.prompt([{
         // get Team members name
         message: "Enter Team Member's name",
         name: "name"
@@ -36,113 +40,66 @@ function addMember() {
     }
 
     ])
-    .then(answers => {
+    .then( async answers => {
         if (answers.role === "Engineer") {
-            inquirer.prompt([
+            await inquirer.prompt([
                 {
                     name: "Github",
                     message: "Please enter The Github Username"
                 }
-            ])
-        }
-        if (answers.role === "Intern") {
+            ]) 
+            .then(addMore)
+           
+
+            
+        } else if (answers.role === "Intern") {
             inquirer.prompt([
                 {
                     name: "school",
                     message: "Please enter the school name"
                 }
             ])
-            if (answers.role === "Manager") {
+             
+        } else  if (answers.role === "Manager") {
                 inquirer.prompt([
                     {
                         name: "officeNumber",
                         message: "Please enter the office number for this employee"
     
                     }
-                ])
+                ])    
             }
-        }
-
+        
+            
     }) 
-    return Answers;
+    
+    return answers;
 }
 
-function generateHtml(data) {
-    `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>My Team</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
-        <script src="https://kit.fontawesome.com/c502137733.js"></script>
-    </head>
-    
-    <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 jumbotron mb-3 team-heading">
-                    <h1 class="text-center">My Team</h1>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="team-area col-12 d-flex justify-content-center">
-                    ${data.team}
-                </div>
-            </div>
-        </div>
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${data.name}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>{{ role }}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: {{ id }}</li>
-            <li class="list-group-item">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/{{ github }}" target="_blank" rel="noopener noreferrer">{{ github }}</a></li>
-        </ul>
-    </div>
-</div>
+ async function addMore() {
+    let add = await inquirer.prompt([
+        {
+            type: "list",
+            name: "addMore",
+            message: "Would you like to add another team member?",
+            choices: ["yes", "no"]
+        }
+    ])
+    .then(answers => {
+    if(answers.addMore === "yes") {
+        return addMember();
+    }
 
-<div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">{{ name }}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>{{ role }}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: {{ id }}</li>
-            <li class="list-group-item">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
-            <li class="list-group-item">School: {{ school }}</li>
-        </ul>
-    </div>
-</div>
-
-<div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">{{ name }}</h2>
-        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>{{ role }}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: {{ id }}</li>
-            <li class="list-group-item">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
-            <li class="list-group-item">Office number: {{ officeNumber }}</li>
-        </ul>
-    </div>
-</div>
-    </body>
-    
-    </html>`
 }
+
+    )
+}
+
+
 addMember();
+
+
+
   
     
 
